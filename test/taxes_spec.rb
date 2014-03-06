42955541a9c40e2338c0b9bb9efdb5006d5170b0
ewhitebloom <ewhitebloom@gmail.com>
2014-03-06 16:01:18 -0500

    test changes

--- test/taxes_spec.rb
+++ test/taxes_spec.rb
@@ -3,18 +3,19 @@ require 'rspec'
 require_relative '../lib/taxes.rb'
 
 describe Taxes do
-  let(:person) { Taxes.new(name, csv_file)}
+  let(:person) { Taxes.new("Jane Doe", '../lib/taxes.csv') }
+  let(:data) {  person.tax_data }
 
   it 'calculates tax absolute value for one person' do
-   person.taxes = person.tax_paid - person.annual_income * person.tax_rate
+   person.tax_calcs("Jane Doe", data)
  end
 
  it 'returns a refund' do
-   expect(person.tax_calcs.to be > 0 )
+  expect(person.tax_calcs(@person, @csv_file).to be > 0 )
  end
 
  it 'returns taxes owed' do
-   expect(person.tax_calcs.to be < 0 )
+   expect(person.tax_calcs(@person, @csv_file).to be < 0 )
  end
 
 end
