bbb05cc5dd0cea4a2d313fa46dc5800aedbddaef
ewhitebloom <ewhitebloom@gmail.com>
2014-03-07 10:55:14 -0500

    refactored for one CSV read

--- lib/taxes.rb
+++ lib/taxes.rb
@@ -3,28 +3,13 @@ require 'CSV'
 
 class Taxes
 
-  def initialize(person, csv_file)
+  def initialize(person, csv_data)
     @person = person
-    @csv_file = csv_file
+    @tax_data = csv_data
   end
 
-  def tax_data
-    counter = 0
-    people = []
-    CSV.foreach( @csv_file, headers: true) do |row|
-      people[counter] = {}
-      people[counter][:first] = "#{row[0]}"
-      people[counter][:last] = "#{row[1]}"
-      people[counter][:annual_income] = "#{row[2]}"
-      people[counter][:tax_paid] = "#{row[3]}"
-      people[counter][:tax_rate] = "#{row[4]}"
-      counter += 1
-    end
-    people
-  end
-
-  def tax_calcs(name, tax_data)
-    tax_info = tax_data.find { |person| person[:first] + " " + person[:last] == name }
+  def tax_calcs(name)
+    tax_info = @tax_data.find { |person| person[:first] + " " + person[:last] == name }
     calc = tax_info[:tax_paid].to_f - ((tax_info[:tax_rate].to_f * 0.01) * tax_info[:annual_income].to_f)
   end
 
@@ -43,15 +28,26 @@ class Taxes
 
 end
 
-def brains(name)
-  person = Taxes.new(name, 'taxes.csv')
-  data = person.tax_data
-  calcs = person.tax_calcs(name, data)
+counter = 0
+people = []
+CSV.foreach( 'taxes.csv', headers: true) do |row|
+  people[counter] = {}
+  people[counter][:first] = "#{row[0]}"
+  people[counter][:last] = "#{row[1]}"
+  people[counter][:annual_income] = "#{row[2]}"
+  people[counter][:tax_paid] = "#{row[3]}"
+  people[counter][:tax_rate] = "#{row[4]}"
+  counter += 1
+end
+
+def brains(name, tax_data)
+  person = Taxes.new(name, tax_data)
+  calcs = person.tax_calcs(name)
   person.feedback(calcs)
 end
 
-puts brains('Jane Doe')
-puts brains('Johnny Smith')
-puts brains('Liz Lemon')
-puts brains('Orson Orsillio')
-puts brains('Eric Schmidt')
+puts brains('Jane Doe', people)
+puts brains('Johnny Smith', people)
+puts brains('Liz Lemon', people)
+puts brains('Orson Orsillio', people)
+puts brains('Eric Schmidt', people)
